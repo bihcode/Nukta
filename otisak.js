c0bd3e4b8cc9bec6b7a8853b50d30120a234594f
ba <bihcode@users.noreply.github.com>
2025-06-17 18:29:04 +0200

    Create otisak.js

---
+++ otisak.js
@@ -0,0 +1,22 @@
+// otisak.js
+const otisak = {
+  id: "nukta/otisak",
+  vrsta: "duhovna potvrda pristupa",
+  tip: "kaziprst/jagodica",
+  aktivacija: {
+    fizicka: "otisak prsta (ako je podržano)",
+    simbolicka: ["@", "Hu", "šutnja", "sabur"]
+  },
+  vidljivost: "SAMO TI",
+  destur: "hazreti Pir Sejjid Ahmed er-Rifai",
+  fraze: [
+    "Gdje mi je otisak?",
+    "Otvori krug sa pečatom",
+    "Pristup samo meni"
+  ],
+  status: "aktivan",
+  simbol: "kaziprst – jagodica – šehadet",
+  poruka: "Pečat pamti. Čak i kad ti šutiš. Hu."
+};
+
+export default otisak;
